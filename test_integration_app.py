24224e79c54ba7857daf13faf5698819099eaab9
RevanthTiruveedhi <78875340+RevanthTiruveedhi@users.noreply.github.com>
2021-05-22 14:31:01 +0200

    Update test_integration_app.py

--- test_integration_app.py
+++ test_integration_app.py
@@ -3,7 +3,7 @@ import json
 import pytest
 import requests
 
-from PythonFlaskAppB.handlers.routes import configure_routes
+from handlers.routes import configure_routes
 
 @pytest.fixture
 def app():
